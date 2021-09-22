d4a266b55f5be0e5d5f78052e183e138256099f8
Simon <simon.altamirano@sjsu.edu>
2021-09-21 19:06:49 -0700

    my main

--- Assignment1/HW1/src/main/Main.java
+++ Assignment1/HW1/src/main/Main.java
@@ -10,13 +10,13 @@ public class Main {
 		Lexer mylexer = new Lexer();
 		while (true) {
 			Token parsedToken = mylexer.getNextToken();
-			
+
 			if (parsedToken == null) {
 				System.out.println("End of Line");
 				break;
 			} else { // prints
 				if (parsedToken.tag >= 256 && parsedToken.tag <= 275) {
-					if(parsedToken.tag == 257){
+					if (parsedToken.tag == 257) {
 						System.out.println("BASE_T:      " + parsedToken.toString());
 						continue;
 					}
