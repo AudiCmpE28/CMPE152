2e2346a5eeccd927a2376572377e6e4eac5cc7d2
Simon <simon.altamirano@sjsu.edu>
2021-09-25 12:24:05 -0700

    tag fix merge

--- HW_1/sourceFile/lexer/Tag.java
+++ HW_1/sourceFile/lexer/Tag.java
@@ -1,12 +1,7 @@
 package lexer;
+
 public class Tag {
       public final static int AND = 256, BASIC = 257, BREAK = 258, DO = 259, ELSE = 260, EQ = 261, FALSE = 262,
-<<<<<<< HEAD
-            GE = 263, ID = 264, IF = 265, INDEX = 266, LE = 267, MINUS = 268, NE = 269, NUM = 270, OR = 271,
-            REAL = 272, TEMP = 273, TRUE = 274, WHILE = 275, EOF = 13;
-}
-=======
                   GE = 263, ID = 264, IF = 265, INDEX = 266, LE = 267, MINUS = 268, NE = 269, NUM = 270, OR = 271,
                   REAL = 272, TEMP = 273, TRUE = 274, WHILE = 275, EOF = 300;
 }
->>>>>>> 1bf5b3ad2c48e32cf94f3894a07542e6749c6071
