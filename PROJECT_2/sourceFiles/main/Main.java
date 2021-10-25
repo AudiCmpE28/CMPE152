750bfaedba706036a8040ea85b77dc8a04c9f90e
Simon <simon.altamirano@sjsu.edu>
2021-10-24 19:12:27 -0700

    new lexer

--- PROJECT_2/sourceFiles/main/Main.java
+++ PROJECT_2/sourceFiles/main/Main.java
@@ -8,7 +8,6 @@ import parser.Parser;
 import java.io.IOException;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		Lexer lex = new Lexer();
 		Parser parse = new Parser(lex);
